9fc2a42065e7b62eab1b6bfab189c1516294bbb8
MartinKratochvilProgramy <martvil96@gmail.com>
2022-12-28 14:40:56 +0100

    deployed server on cyclic

--- src/serverRoute.ts
+++ src/serverRoute.ts
@@ -1,4 +1,4 @@
-export const serverRoute = 'https://dca-calculator.onrender.com'
+export const serverRoute = 'https://dca-calculator.cyclic.app'
 
 // //localhost:3000
 // https://dca-calculator.onrender.com
